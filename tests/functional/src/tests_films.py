846910a6d555ccec2dfa4365d0f1762a85df7397
Andrey <and.isaev.v@yandex.ru>
2021-12-21 12:37:31 +0300

    первые тесты для фильмов

---
+++ tests/functional/src/tests_films.py
@@ -0,0 +1,26 @@
+import pytest
+
+from testdata.search_parametrs import film_search_params
+from utils.checkpoints_status import check_results
+
+
+@pytest.mark.parametrize(
+    "path, query, status, page_size",
+    [
+        *film_search_params,
+    ]
+)
+@pytest.mark.asyncio
+async def test_get_film_search(
+        make_get_request,
+        path: str,
+        query: dict,
+        status: int,
+        page_size: int):
+    response = await make_get_request(f"/{path}", query)
+    check_results(
+        status=status,
+        expected_status=response.status,
+        body=response.body,
+        expected_page_size=page_size
+    )
---
+++ tests/settings.py
@@ -0,0 +1,7 @@
+
+
+class Settings:
+    SERVICE_URL: str = 'http://127.0.0.1:8000'
+    PERSON_INDEX: str = 'test_person'
+    GENRE_INDEX: str = 'test_genre'
+    MOVIES_INDEX: str = 'test_movies'
